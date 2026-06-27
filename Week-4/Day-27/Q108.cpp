0af836bd6f0fe02382db47c90080550893d0a837
Vidit Singh <viditsingh558@gmail.com>
2026-06-27 12:48:31 +0530

    updated 108

--- Week-4/Day-27/Q108.cpp
+++ Week-4/Day-27/Q108.cpp
@@ -8,7 +8,7 @@ int main() {
     cout << "Enter number of students: ";
     cin >> n;
 
-    // Arrays to store roll numbers, names, and marks
+    // Arrays to store student details
     int rollnumber[n];
     char name[100][50];
     float maths[n];
@@ -20,34 +20,59 @@ int main() {
     // Input student details
     for(int i = 0; i < n; i++) {
 
+        // Input roll number
         cout << "Enter the roll number of " << i + 1 << " student: ";
         cin >> rollnumber[i];
 
-        // Remove the newline character left by cin
+        // Remove newline left by cin before getline
         cin.ignore();
 
+        // Input student name
         cout << "Enter the name of " << i + 1 << " student: ";
         cin.getline(name[i], 50);
 
+        // Input Maths marks
         cout << "Enter the maths marks of " << i + 1 << " student: ";
         cin >> maths[i];
 
+        // Input Physics marks
         cout << "Enter the physics marks of " << i + 1 << " student: ";
         cin >> physics[i];
 
+        // Input Chemistry marks
         cout << "Enter the chemistry marks of " << i + 1 << " student: ";
         cin >> chemistry[i];
 
-        total[i]=maths[i]+physics[i]+chemistry[i];
-        percentage[i]=total[i]/3;
+        // Calculate total marks
+        total[i] = maths[i] + physics[i] + chemistry[i];
+
+        // Calculate percentage
+        percentage[i] = total[i] / 3;
     }
 
-    // Display student records
+    // Display student marksheet
+    cout << "\nStudent Marksheet Records\n";
+
     for(int i = 0; i < n; i++) {
-        cout << i + 1 << " student name is " << name[i] << endl;
-        cout << i + 1 << " student roll number is " << rollnumber[i] << endl;
-        cout << i + 1 << " student total marks is " << total[i] << endl;
-        cout << i + 1 << " student percentage  is " << percentage[i]<<" %" << endl;
+
+        cout << "\nStudent " << i + 1 << " Details" << endl;
+
+        // Display student name
+        cout << "Name: " << name[i] << endl;
+
+        // Display roll number
+        cout << "Roll Number: " << rollnumber[i] << endl;
+
+        // Display subject marks
+        cout << "Maths Marks: " << maths[i] << endl;
+        cout << "Physics Marks: " << physics[i] << endl;
+        cout << "Chemistry Marks: " << chemistry[i] << endl;
+
+        // Display total marks
+        cout << "Total Marks: " << total[i] << endl;
+
+        // Display percentage
+        cout << "Percentage: " << percentage[i] << "%" << endl;
     }
 
     return 0;
